2dc627e0004282236dfd09102d23b6ca5e94adf2
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-03-13 15:22:02 +0100

    add gitignore

--- runplaybook.sh
+++ runplaybook.sh
@@ -2,7 +2,7 @@
 # Configure STDOUT_CALLBACK if -v is attached to the command
 # so the output is better readable during troubleshooting
 
-echo -n "$@" | grep -q -- "-v" && export ANSIBLE_STDOUT_CALLBACK=yaml
+echo -n "$@" | grep -q -- "-v" && export ANSIBLE_STDOUT_CALLBACK=dense
 
 ansible-playbook playbook.yml --ask-become-pass
 
